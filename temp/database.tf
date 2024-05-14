f835027da151a1fb75602503c57990b12222194f
junho100 <bemodesty306@naver.com>
2024-05-14 15:15:59 +0900

    Feat: add mongodb, redis config

--- temp/database.tf
+++ temp/database.tf
@@ -5,5 +5,15 @@ resource "linode_instance" "database" {
   type      = "g6-standard-2"
   root_pass = var.instance_password
 
-  stackscript_id = linode_stackscript.install_docker_postgresql.id
+  stackscript_id = linode_stackscript.install_docker_postgresql_mongo.id
+}
+
+resource "linode_instance" "redis" {
+  label     = format(module.naming.result, "redis")
+  image     = "linode/ubuntu22.04"
+  region    = "jp-osa"
+  type      = "g6-standard-1"
+  root_pass = var.instance_password
+
+  stackscript_id = linode_stackscript.install_docker_redis.id
 }
--- temp/stack-script.tf
+++ temp/stack-script.tf
@@ -1,8 +1,16 @@
-resource "linode_stackscript" "install_docker_postgresql" {
+resource "linode_stackscript" "install_docker_postgresql_mongo" {
   label       = format(module.naming.result, "install-docker-postgresql")
   description = "install docker and run postgresql"
-  script = templatefile("./files/create-postgresql.sh.tftpl", {
+  script = templatefile("./files/create-postgresql-mongo.sh.tftpl", {
     password = var.db_password
+  })
+  images = ["linode/ubuntu22.04"]
+}
+
+resource "linode_stackscript" "install_docker_redis" {
+  label       = format(module.naming.result, "install-docker-redis")
+  description = "install docker and run redis"
+  script = templatefile("./files/create-redis.sh.tftpl", {
   })
   images = ["linode/ubuntu22.04"]
 }

f2f606a1a3fec45041f556cf6dca5e30b0686b99
junho100 <bemodesty306@naver.com>
2024-06-17 12:37:17 +0900

    Fix: update redis engine version

--- prod/elasticache.tf
+++ prod/elasticache.tf
@@ -3,8 +3,8 @@ resource "aws_elasticache_cluster" "elasticache" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.1"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.elasticache_subnet_group.name
 

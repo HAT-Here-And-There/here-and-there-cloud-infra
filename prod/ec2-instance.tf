6201586a5a02579721c66ad605c2ed968a4a593c
junho100 <bemodesty306@naver.com>
2024-06-17 12:34:04 +0900

    Fix: add bastion instance type

--- prod/ec2-instance.tf
+++ prod/ec2-instance.tf
@@ -4,7 +4,7 @@ module "bastion_host" {
   name = format(module.naming.result, "bastion-host-ec2")
 
   ami                         = "ami-0b23bb3616e3892a6"
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   availability_zone           = element(module.vpc.azs, 0)
   subnet_id                   = element(module.vpc.public_subnets, 0)
   vpc_security_group_ids      = [module.sg_for_bastion_host.security_group_id]
